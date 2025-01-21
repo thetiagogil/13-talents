410406afbe889a86259f319fe7db76b38f212406
thetiagogil <talmeidagil@hotmail.com>
2025-01-21 15:15:27 +0000

    optimize components

---
+++ src/pages/home.page.tsx
@@ -0,0 +1,10 @@
+import { Typography } from "@mui/joy";
+import { Layout } from "../components/shared/containers";
+
+export const HomePage = () => {
+  return (
+    <Layout>
+      <Typography>Hello World</Typography>
+    </Layout>
+  );
+};
