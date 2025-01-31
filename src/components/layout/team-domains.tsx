e650f90b1951fb48f29cb1a7cc8420e7478406f6
thetiagogil <talmeidagil@hotmail.com>
2025-01-31 14:08:13 +0000

    add team domains component to the team overview tab

---
+++ src/components/layout/team-domains.tsx
@@ -0,0 +1,55 @@
+import { Card, Stack, Typography } from "@mui/joy";
+import { STRENGTH_CATEGORIES } from "../../lib/constants";
+import { StrengthModel } from "../../models/strength.model";
+import { UsersStrengthsModel } from "../../models/users-strengths.model";
+import { getColorHex } from "../../utils/get-color-hex";
+
+type TeamSearchProps = {
+  strengths: StrengthModel[];
+  usersStrengths: UsersStrengthsModel[];
+};
+
+export const TeamDomains = ({ strengths, usersStrengths }: TeamSearchProps) => (
+  <Stack gap={2.5}>
+    <Typography level="title-lg" fontWeight={700}>
+      Team's Domains
+    </Typography>
+
+    <Stack direction="row">
+      {STRENGTH_CATEGORIES.map((category, index) => {
+        const filterStrengthsByCategory = strengths.filter(strength => strength.category === category);
+        const filterUsersStrengthsByCategory = usersStrengths.filter(item =>
+          filterStrengthsByCategory.some(strength => item.strength_id === strength.id)
+        );
+        const numberOfUsers = new Set(filterUsersStrengthsByCategory.map(item => item.user_id)).size;
+        const numberOfStrengths = new Set(filterUsersStrengthsByCategory.map(item => item.strength_id)).size;
+
+        return (
+          <Card
+            key={category}
+            variant="plain"
+            sx={{
+              bgcolor: getColorHex(category),
+              flex: 1,
+              borderRadius: 0,
+              ...(index === 0 && { borderTopLeftRadius: 8, borderBottomLeftRadius: 8 }),
+              ...(index === 3 && { borderTopRightRadius: 8, borderBottomRightRadius: 8 })
+            }}
+          >
+            <Stack gap={1}>
+              <Typography level="body-md" textColor="neutral.white" fontWeight={700}>
+                {category}
+              </Typography>
+              <Typography level="body-md" textColor="neutral.white">
+                {numberOfUsers} people
+              </Typography>
+              <Typography level="body-md" textColor="neutral.white">
+                {numberOfStrengths} strengths
+              </Typography>
+            </Stack>
+          </Card>
+        );
+      })}
+    </Stack>
+  </Stack>
+);
