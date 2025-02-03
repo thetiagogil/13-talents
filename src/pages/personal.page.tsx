082b357f5613950851a93cf8f5aca1ab8cec2f31
thetiagogil <talmeidagil@hotmail.com>
2025-02-03 16:37:39 +0000

    add profile and manual tabs to team overview and update search mechanic

--- src/pages/personal.page.tsx
+++ src/pages/personal.page.tsx
@@ -3,18 +3,19 @@ import { MainContainer } from "../components/shared/main-container";
 import { TabsContainer } from "../components/shared/tabs-container";
 import { PersonalActivityTab } from "../components/tabs/personal-activity.tab";
 import { PersonalGoalsTab } from "../components/tabs/personal-goals.tab";
-import { PersonalManualTab } from "../components/tabs/personal-manual.tab";
-import { PersonalProfileTab } from "../components/tabs/personal-profile.tab";
+import { SharedManualTab } from "../components/tabs/shared-manual.tab";
+import { SharedProfileTab } from "../components/tabs/shared-profile.tab";
 
 export const PersonalPage = () => {
   const [activeTab, setActiveTab] = useState<string | number | null>(0);
+
   return (
     <MainContainer hasSideBar>
       <TabsContainer
         hasPadding
         tabs={[
-          { tab: "My profile", panel: <PersonalProfileTab /> },
-          { tab: "Manual of me", panel: <PersonalManualTab /> },
+          { tab: "My profile", panel: <SharedProfileTab /> },
+          { tab: "Manual of me", panel: <SharedManualTab /> },
           { tab: "Personal goals", panel: <PersonalGoalsTab /> },
           { tab: "Activity", panel: <PersonalActivityTab />, disabled: true }
         ]}
